107aeb9ec04cb3c76bdd896085882e9eea7a8301
Mickae Truman <mtruman@nyit.edu>
2019-03-15 02:36:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,9 @@ def self.alphabetical
   self.all.sort_by{|song| song.name}
 end
 
-def self.create_from_filename(name)
+
+
+def self.new_from_filename(name)
   song = self.new 
   song.name = (name.split("-")[1].chomp(".mp3"))
   song.artist_name = (name.split("-")[0])
