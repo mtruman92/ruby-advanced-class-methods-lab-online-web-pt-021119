f6451b0e0fb0eb52508a0cda3d82ae18df4ef217
Mickae Truman <mtruman@nyit.edu>
2019-03-15 02:49:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,8 @@ def self.find_by_name(song_name)
   self.all.find == song_name
   end
   
-def self.find_or_create_by_name(song_name)
-  self.find_by_name(song_name) || self.create_by_name(song_name)
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
 end
 
 def self.alphabetical
